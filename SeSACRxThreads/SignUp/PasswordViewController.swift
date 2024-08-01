2c8085d459de9c0c69055e645ab9ad9812ea58c3
Alpaca38 <137505484+Alpaca38@users.noreply.github.com>
2024-08-01 20:04:34 +0900

    PhoneViewController

--- SeSACRxThreads/SignUp/PasswordViewController.swift
+++ SeSACRxThreads/SignUp/PasswordViewController.swift
@@ -7,11 +7,16 @@
 
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class PasswordViewController: UIViewController {
    
     let passwordTextField = SignTextField(placeholderText: "비밀번호를 입력해주세요")
     let nextButton = PointButton(title: "다음")
+    private let descriptionLabel = UILabel()
+    private let validText = Observable.just("8자 이상 입력해주세요.")
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,16 +24,12 @@ class PasswordViewController: UIViewController {
         view.backgroundColor = Color.white
         
         configureLayout()
-         
-        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
-    }
-    
-    @objc func nextButtonClicked() {
-        navigationController?.pushViewController(PhoneViewController(), animated: true)
+        bind()
     }
     
     func configureLayout() {
         view.addSubview(passwordTextField)
+        view.addSubview(descriptionLabel)
         view.addSubview(nextButton)
          
         passwordTextField.snp.makeConstraints { make in
@@ -37,11 +38,40 @@ class PasswordViewController: UIViewController {
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
         
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(passwordTextField.snp.bottom)
+            $0.horizontalEdges.equalTo(passwordTextField)
+            $0.height.equalTo(20)
+        }
+        
         nextButton.snp.makeConstraints { make in
             make.height.equalTo(50)
-            make.top.equalTo(passwordTextField.snp.bottom).offset(30)
+            make.top.equalTo(descriptionLabel.snp.bottom).offset(20)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
     }
 
+    func bind() {
+        disposeBag.insert {
+            validText
+                .bind(to: descriptionLabel.rx.text)
+            
+            let validation = passwordTextField.rx.text.orEmpty
+                .map { $0.count >= 8 }
+            
+            validation
+                .bind(to: nextButton.rx.isEnabled, descriptionLabel.rx.isHidden)
+            
+            validation
+                .bind(with: self) { owner, value in
+                    let color: UIColor = value ? .systemPink : .lightGray
+                    owner.nextButton.backgroundColor = color
+                }
+            
+            nextButton.rx.tap
+                .bind(with: self) { owner, _ in
+                    owner.navigationController?.pushViewController(PhoneViewController(), animated: true)
+                }
+        }
+    }
 }
--- SeSACRxThreads/SignUpOptional/PhoneViewController.swift
+++ SeSACRxThreads/SignUpOptional/PhoneViewController.swift
@@ -7,11 +7,19 @@
  
 import UIKit
 import SnapKit
+import RxSwift
+import RxCocoa
 
 class PhoneViewController: UIViewController {
    
     let phoneTextField = SignTextField(placeholderText: "연락처를 입력해주세요")
     let nextButton = PointButton(title: "다음")
+    
+    private let descriptionLabel = UILabel()
+//    private let validText = Observable.just("10자 이상 입력해주세요.")
+    private let validText = BehaviorRelay(value: "10자 이상 입력해주세요")
+    private let phoneText = Observable.just("010")
+    private let disposeBag = DisposeBag()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,17 +27,12 @@ class PhoneViewController: UIViewController {
         view.backgroundColor = Color.white
         
         configureLayout()
-        
-        nextButton.addTarget(self, action: #selector(nextButtonClicked), for: .touchUpInside)
+        bind()
     }
-    
-    @objc func nextButtonClicked() {
-        navigationController?.pushViewController(NicknameViewController(), animated: true)
-    }
-
     
     func configureLayout() {
         view.addSubview(phoneTextField)
+        view.addSubview(descriptionLabel)
         view.addSubview(nextButton)
          
         phoneTextField.snp.makeConstraints { make in
@@ -38,11 +41,67 @@ class PhoneViewController: UIViewController {
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
         
+        descriptionLabel.snp.makeConstraints {
+            $0.top.equalTo(phoneTextField.snp.bottom)
+            $0.horizontalEdges.equalTo(phoneTextField)
+            $0.height.equalTo(20)
+        }
+        
         nextButton.snp.makeConstraints { make in
             make.height.equalTo(50)
-            make.top.equalTo(phoneTextField.snp.bottom).offset(30)
+            make.top.equalTo(descriptionLabel.snp.bottom).offset(20)
             make.horizontalEdges.equalTo(view.safeAreaLayoutGuide).inset(20)
         }
     }
 
+}
+
+private extension PhoneViewController {
+    func bind() {
+        disposeBag.insert {
+            nextButton.rx.tap
+                .bind(with: self) { owner, _ in
+                    owner.navigationController?.pushViewController(NicknameViewController(), animated: true)
+                }
+            
+            phoneText
+                .bind(to: phoneTextField.rx.text)
+            
+            validText
+                .bind(to: descriptionLabel.rx.text)
+            
+            let countValidation = phoneTextField.rx.text.orEmpty
+                .map { $0.count >= 10 }
+            
+            countValidation
+                .bind(with: self) { owner, value in
+                    if value == false {
+                        owner.validText.accept("10자 이상 입력해주세요.")
+                    }
+                }
+            
+            let numberValidation = phoneTextField.rx.text.orEmpty
+                .map { Int($0) != nil }
+            
+            numberValidation
+                .bind(with: self) { owner, value in
+                    if value == false {
+                        owner.validText.accept("숫자만 입력해주세요.")
+                    }
+                }
+            
+            let totalValidation = Observable.zip(countValidation, numberValidation)
+                .map { $0 && $1 }
+            
+            totalValidation
+                .bind(to: nextButton.rx.isEnabled, descriptionLabel.rx.isHidden)
+            
+            totalValidation
+                .bind(with: self) { owner, value in
+                    let color: UIColor = value ? .systemPink : .lightGray
+                    owner.nextButton.backgroundColor = color
+                }
+            
+        }
+    }
 }
